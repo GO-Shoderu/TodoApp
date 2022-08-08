cee876207ea4c3f5bcce22ea08541e04722236d9
GO-Shoderu <go.shoderu@gmail.com>
2022-08-08 08:51:47 +0200

    Updated the view to have a delete button and also added controller for the delete button

--- app.py
+++ app.py
@@ -16,10 +16,11 @@ class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.BOOLEAN, nullable=False)
 
     # debugging helper
     def __repr__(self):
-        return f'<Todo {self.id} {self.description}>'
+        return f'<Todo {self.id} {self.description} {self.completed}>'
 
 db.create_all()
 
@@ -28,7 +29,8 @@ db.create_all()
 # this listens to the homepage
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.all())     #this render_template returns an index.html
+    #this render_template returns an index.html
+    return render_template('index.html', data=Todo.query.order_by('id').all())     
 
 # this listens to the create
 @app.route('/todos/create', methods=['POST'])
@@ -39,7 +41,7 @@ def create_todo():
     try:
         # description = request.form.get('description', '')
         description = request.get_json()['description']
-        todo = Todo(description = description)
+        todo = Todo(description = description, completed = False)
         db.session.add(todo)
         db.session.commit()
         body['description'] = todo.description
@@ -57,6 +59,48 @@ def create_todo():
         
         # return redirect(url_for('index'))
         # return description + ' Todo Created!!!'
+
+# this listens to the on checked function
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    # error = False
+    # body = {}
+    # print('todo id is ' + todo_id)
+    # print('complete status for id ' + todo_id + ' is' + request.get_json()['completed'])
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.add(todo)
+        db.session.commit()
+        # body['completed'] = todo.completed
+    except:
+        # error = True
+        db.session.rollback()
+        # print(sys.exc_info())
+    finally:
+        db.session.close()
+    
+    return redirect(url_for('index'))
+
+    # if error:
+    #     abort(400)
+    # else:
+    #     return jsonify(body)
+
+# this listens to the deled button function
+@app.route('/todos/<todo_id>/delete-todo', methods=['GET'])
+def delete_todo(todo_id):
+    try:
+        todo = Todo.query.get(todo_id)
+        db.session.delete(todo)
+        db.session.commit()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+
+    return redirect(url_for('index'))
 
 #######################################################
 
